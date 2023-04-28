36cd7af3386f26aa04c31bd4470302a1567411bc
Pedro Folkmann <pedro.o.folkmann@gmail.com>
2023-04-28 10:54:09 -0300

    Atualização foto

--- src/pages/components/cont.tsx
+++ src/pages/components/cont.tsx
@@ -1,5 +1,7 @@
-import img1 from "../../img/Quadra1.jpg"
-import img2 from "../../img/Quadra2.jpg"
+import quadra1 from "../../img/Quadra1.jpg"
+import quadra2 from "../../img/Quadra2.jpg"
+import campo1 from "../../img/Campo.jpg"
+import copa from "../../img/CopaMaua.jpg"
 
 import Image from "next/image"
 
@@ -9,7 +11,7 @@ export default function Cont() {
             <div className="flex-col my-5 space-y-5 mx-3">
                 <div className="sm:flex space-y-5 sm:space-y-0">
                     <div className="bg-mauaBrown ml-2.5 mr-5 rounded-3xl shadow-md flex flex-col md:flex-row sm:w-1/2 ">
-                        <Image src={img1} className="w-340 m-auto  sm:my-auto rounded-3xl" alt="Quadra" />
+                        <Image src={quadra1} className="w-340 m-auto  sm:my-auto rounded-3xl" alt="Quadra" />
                         <div className="ml-5 mt-5 mr-5 text-center sm:text-left">
                             <h3>Quadras</h3>
                             <p className="pb-5">
@@ -22,7 +24,7 @@ export default function Cont() {
                     </div>
 
                     <div className="bg-mauaBrown ml-2.5 mr-5 rounded-3xl shadow-md flex flex-col md:flex-row sm:w-1/2 ">
-                        <Image src={img2} className="w-340 m-auto  sm:my-auto rounded-3xl" alt="Quadra" />
+                        <Image src={campo1} className="w-340 m-auto  sm:my-auto rounded-3xl" alt="Quadra" />
                         <div className="ml-5 mt-5 mr-5 text-center sm:text-left">
                             <h3>Campo</h3>
                             <p className="pb-5">
@@ -34,7 +36,7 @@ export default function Cont() {
                     </div>
                 </div>
                 <div className="bg-mauaBrown ml-2.5 mr-5 flex flex-col rounded-3xl shadow-md sm:flex-row">
-                    <Image src={img2} className=" w-340 sm:justify-center lg:justify-right rounded-3xl" alt="Quadra" />
+                    <Image src={copa} className=" w-340 sm:justify-center lg:justify-right rounded-3xl" alt="Quadra" />
                     <div className="ml-5 mt-5 mr-5  text-center sm:text-left">
                         <h3>Copa Maua</h3>
                         <p className=" align-middle items-center pb-5 sm:mx-10">
