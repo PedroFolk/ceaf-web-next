2047d1bb6cece8e3c33639c6c2fe586c14e026bb
Pedro Folkmann <pedro.o.folkmann@gmail.com>
2023-05-05 11:59:00 -0300

    Correções Menores

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,18 +1,13 @@
 import Header from './components/header'
 import Cont from './components/cont'
-
-const links = [
-  { name: "Inicio", link: "/user" },
-  { name: "Reservas", link: "/reservas" },
-  { name: "Regras", link: "/regras" },
-];
+import {links, linksLog} from './components/constants'
 
 function Home() {
   
   return (
     <>
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      <Header links={links} />
+      <Header links={links} linksLog={linksLog}/>
       <Cont />
     </>
   );
