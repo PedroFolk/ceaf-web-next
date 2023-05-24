9d7af2307124fb1d0a63b2d12ca6b7715be2b59e
Pedro Folkmann <pedro.o.folkmann@gmail.com>
2023-05-24 10:20:21 -0300

    Correções

--- src/pages/components/reservas.tsx
+++ src/pages/components/reservas.tsx
@@ -1,30 +1,41 @@
 import Image from "next/image";
-import campo from "../../img/CopaMaua.jpg"
+import campo from "../../img/CopaMaua.jpg";
+
+interface Reserva {
+    dia: string;
+    hora: string;
+    local: string;
+}
 
 interface ReservasProps {
-    reserva: { dia: string, hora: string, local: string }[];
+    reserva: Reserva[];
 }
 
 export default function Reservas({ reserva }: ReservasProps) {
+    if (!reserva || !Array.isArray(reserva)) {
+        return null; // Or handle the case when reserva is undefined or not an array
+    }
+
     return (
-        <div >
-            {reserva.map((reserva, index) => (
+        <div>
+            {reserva.map((item, index) => (
                 <div key={index}>
                     <div className="flex flex-col bg-mauaBrown m-auto space-y-6 w-1/3 text-center my-4 p-2 rounded-2xl">
-                        <div className=" m-auto">
-                            <Image className="rounded-2xl" src={campo} alt={"Reserva"} />
+                        <div className="m-auto">
+                            <Image className="rounded-2xl" src={campo} alt="Reserva" />
                         </div>
-                        <div className=" flex flex-col space-y-2 justify-center sm:space-y-4">
+                        <div className="flex flex-col space-y-2 justify-center sm:space-y-4">
                             <h1 className="text-2xl flex-nowrap">
-                                <span className="text-2xl font-bold">Horario:</span> Dia {reserva.dia} as {reserva.hora}
+                                <span className="text-2xl font-bold">Horario:</span> Dia{" "}
+                                {item.dia} as {item.hora}
                             </h1>
                             <h1 className="text-2xl">
-                                <span className="text-2xl font-bold">Local:</span> {reserva.local}
+                                <span className="text-2xl font-bold">Local:</span>{" "}
+                                {item.local}
                             </h1>
-
                         </div>
-                        <div className=" flex flex-col justify-center space-y-4">
-                            <h1 className=" text-2xl ">
+                        <div className="flex flex-col justify-center space-y-4">
+                            <h1 className="text-2xl">
                                 <span className="font-bold">Status:</span> Ok
                             </h1>
                         </div>
